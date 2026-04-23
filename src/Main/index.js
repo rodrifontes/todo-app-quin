44e779b3fa28cf848bfbe0d51745429e00998af4
Rodrigo Fontes <rodrifontes@gmail.com>
2026-04-23 18:38:52 -0300

    feat: Criação Projeto, Configuração Fonts , SafeAreaView e Add Component Header

---
+++ src/Main/index.js
@@ -0,0 +1,10 @@
+import Header from '../components/Header';
+import { Container } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
